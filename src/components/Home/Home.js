f6b0f8a4f8f3b73ed886923f4ce65dabc917742f
Kishan Aghera <55836816+kishan-aghera@users.noreply.github.com>
2022-03-26 14:15:08 +0530

    Solved Component not rendered and formatted js files.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './Home.css';
+import React from 'react'
+import './Home.css'
 
-const Home=()=>{
-return (
+const Home = () => {
+  return (
     <div>
-        <h1>I am the Home Page</h1>
+      <h1>I am the Home Page</h1>
     </div>
-);
+  )
 }
 
-export default Home;
+export default Home
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,12 +1,12 @@
-import React from "react";
-import './Skills.css';
+import React from 'react'
+import './Skills.css'
 
-const Skills=()=>{
-    return (
-        <div>
-            <h1>I am the Skills Page</h1>
-        </div>
-    );
+const Skills = () => {
+  return (
+    <div>
+      <h1>I am the Skills Page</h1>
+    </div>
+  )
 }
 
-export default Skills;
+export default Skills
